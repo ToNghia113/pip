7d0b22958a6c1e61ee559f083e4b914e99b82344
ToNghia113 <111074806+ToNghia113@users.noreply.github.com>
2022-10-03 22:28:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-sudo apt -y update; sudo apt -y install screen git nodejs
-npm i fake-useragent
-npm i randomstring
-npm i request
-pip3 install -r requirements.txt
-python3 LTN.py
+
+pip3 install pystyle
+python SPAM.py
